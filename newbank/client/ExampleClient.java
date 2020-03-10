78cac62a50081e259bfb2703bc0a55c8c6cd1a3d
tp757 <tp757@bath.ac.uk>
2020-03-10 23:47:00 +0000

    Stopped repeated nulls and asked for logon details 3 times

--- newbank/client/ExampleClient.java
+++ newbank/client/ExampleClient.java
@@ -7,51 +7,59 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class ExampleClient extends Thread{
-  
+public class ExampleClient extends Thread {
+
   private Socket server;
-  private PrintWriter bankServerOut;  
+  private PrintWriter bankServerOut;
   private BufferedReader userInput;
   private Thread bankServerResponceThread;
-  
+
   public ExampleClient(String ip, int port) throws UnknownHostException, IOException {
-    server = new Socket(ip,port);
-    userInput = new BufferedReader(new InputStreamReader(System.in)); 
-    bankServerOut = new PrintWriter(server.getOutputStream(), true); 
-    
-    bankServerResponceThread = new Thread() {
-      private BufferedReader bankServerIn = new BufferedReader(new InputStreamReader(server.getInputStream())); 
-      public void run() {
-        try {
-          while(true) {
-            String responce = bankServerIn.readLine();
-            System.out.println(responce);
+    server = new Socket(ip, port);
+    userInput = new BufferedReader(new InputStreamReader(System.in));
+    bankServerOut = new PrintWriter(server.getOutputStream(), true);
+
+    bankServerResponceThread =
+        new Thread() {
+          private BufferedReader bankServerIn =
+              new BufferedReader(new InputStreamReader(server.getInputStream()));
+
+          public void run() {
+            try {
+              while (true) {
+                String responce = bankServerIn.readLine();
+                // Stop repeated nulls from printing
+                if (responce == null) {
+                  Thread.currentThread().interrupt();
+                  System.exit(1);
+                }
+                System.out.println(responce);
+              }
+            } catch (IOException e) {
+              e.printStackTrace();
+              return;
+            }
           }
-        } catch (IOException e) {
-          e.printStackTrace();
-          return;
-        }
-      }
-    };
+        };
     bankServerResponceThread.start();
   }
-  
-  
+
   public void run() {
-    while(true) {
+    while (true) {
       try {
-        while(true) {
+        while (true) {
           String command = userInput.readLine();
           bankServerOut.println(command);
-        }        
+        }
       } catch (IOException e) {
         // TODO Auto-generated catch block
         e.printStackTrace();
       }
     }
   }
-  
-  public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
-    new ExampleClient("localhost",14002).start();
+
+  public static void main(String[] args)
+      throws UnknownHostException, IOException, InterruptedException {
+    new ExampleClient("localhost", 14002).start();
   }
 }
---
+++ newbank/client/out/production/NewBank/README.md
@@ -0,0 +1,31 @@
+# NewBank
+Group G repository for NewBank project
+
+## General information
+
+### Source code files
+
+Source code shall conform to the Google Java style guide:
+https://google.github.io/styleguide/javaguide.html
+
+A Google Java format plugin is available for various IDEs to automatically enforce the style guide.  More details here if you wish to use it:
+https://github.com/google/google-java-format
+
+Please ensure your source code editor settings are as follows:  
+ * Source file encoding: UTF-8  
+ * Indentation/tabbing: 2 spaces  
+ * Java version: Java SE 11
+
+### Sprint management
+
+The master branch shall be protected such that it can only be modified via pull request requiring a review, so commits cannot be pushed to it directly in error.
+
+At the start of each new sprint the scrum master will create a sprint branch from master.  This branch shall be protected such that it can only be modified via pull request requiring a review, so commits cannot be pushed to it directly in error.
+
+The scrum team members shall create their own unprotected working branches from the sprint branch.  When development of a feature or bug fix is complete, the scrum team member shall create a pull request to merge their working branch back into the sprint branch which will be reviewed by the scrum master prior to committing the merge.  The scrum master will inform the scrum team of any updates to the sprint branch during a sprint.
+
+At the end of the sprint, the scrum master shall merge the sprint branch into master.
+
+### Help
+
+Any problems, suggestions or questions please contact the scrum master.
