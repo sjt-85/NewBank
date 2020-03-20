3a1cd3162c78ed9a577e8cdac6ad3172324c3b2f
Cornelia Hebrank <ch2333@bath.ac.uk>
2020-03-20 22:09:39 +0100

    Added testcases for NEWACCOUNT with and without currency present

--- newbank/test/ServerTestScenarios.java
+++ newbank/test/ServerTestScenarios.java
@@ -26,4 +26,34 @@ public class ServerTestScenarios {
 
     newbank.test.NBUnit.AssertEqual("Savings: 1500.00 GBP" + System.lineSeparator(), christina);
   }
+
+  @newbank.test.NBUnit.Test
+  private void createNewAccountWithOnlyAccountNameReturnsSuccess() {
+    String john =
+            NewBank.getBank()
+                    .processRequest(NewBank.getBank().checkLogInDetails("John", "3"), "NEWACCOUNT Savings");
+
+    newbank.test.NBUnit.AssertEqual("SUCCESS", john);
+  }
+
+  @newbank.test.NBUnit.Test
+  private void createNewAccountWithAccountNameAndAcceptedCurrencyReturnsSuccess() {
+    String john =
+            NewBank.getBank()
+                    .processRequest(NewBank.getBank().checkLogInDetails("John", "3"), "NEWACCOUNT Travel eur");
+
+    newbank.test.NBUnit.AssertEqual("SUCCESS", john);
+  }
+
+  @newbank.test.NBUnit.Test
+  private void createNewAccountWithWrongCurrencyReturnsFailWithMessage() {
+    String christina =
+            NewBank.getBank()
+                    .processRequest(
+                            NewBank.getBank().checkLogInDetails("Christina", "2"), "NEWACCOUNT Other sar");
+
+    newbank.test.NBUnit.AssertEqual("FAIL: Currency not allowed. Accepted currencies: GBP, EUR, USD ", christina);
+  }
+
+
 }
