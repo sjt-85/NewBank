d469f8b70202ff6b24656c31fe4ead28e9dce9be
st2180 <st2180@bath.ac.uk>
2020-03-21 20:26:15 +0000

    1. Make NewBankClientHandler to be testable. 2. Implement a test for the login sequence.

--- newbank/test/ServerTestScenarios.java
+++ newbank/test/ServerTestScenarios.java
@@ -2,6 +2,11 @@ package newbank.test;
 
 import newbank.server.NewBank;
 
+import java.io.*;
+
+import static newbank.test.NBUnit.Assert;
+import static newbank.test.NBUnit.AssertEqual;
+
 // How to implement test:
 // 1. Define a class
 // 2. Define a test method which:
@@ -11,19 +16,61 @@ import newbank.server.NewBank;
 public class ServerTestScenarios {
 
   @newbank.test.NBUnit.Test
-  private void ShowMyAccountsReturnsListOfAllCustomersAccountsAlongWithCurrentBalance() {
+  private void showMyAccountsReturnsListOfAllCustomersAccountsAlongWithCurrentBalance() {
 
     String bhagy =
         NewBank.getBank()
             .processRequest(NewBank.getBank().checkLogInDetails("Bhagy", "1"), "SHOWMYACCOUNTS");
 
-    newbank.test.NBUnit.AssertEqual("Main: 1000.0" + System.lineSeparator(), bhagy);
+    AssertEqual("Main: 1000.0" + System.lineSeparator(), bhagy);
 
     String christina =
         NewBank.getBank()
             .processRequest(
                 NewBank.getBank().checkLogInDetails("Christina", "2"), "SHOWMYACCOUNTS");
 
-    newbank.test.NBUnit.AssertEqual("Savings: 1500.0" + System.lineSeparator(), christina);
+    AssertEqual("Savings: 1500.0" + System.lineSeparator(), christina);
+  }
+
+  @newbank.test.NBUnit.Test
+  private void testLoginSequence() {
+
+    String userName = "Bhagy";
+    String password = "1";
+
+    String outputString = runServer(userName, password, "");
+
+    final String initialResponse =
+        "Enter Username"
+            + System.lineSeparator()
+            + "Enter Password"
+            + System.lineSeparator()
+            + "Checking Details..."
+            + System.lineSeparator()
+            + "Log In Successful. What do you want to do?"
+            + System.lineSeparator();
+
+    AssertEqual(initialResponse, outputString);
+  }
+
+  private static String runServer(String userName, String password, String command) {
+
+    String inputString =
+        userName + "\n" + password + "\n" + (command + (command.length() == 0 ? "" : "\n"));
+
+    var outputStream = new ByteArrayOutputStream();
+    var writer = new PrintWriter(outputStream);
+
+    var target =
+        new newbank.server.NewBankClientHandler.ClientThreadTarget(
+            new BufferedReader(
+                new InputStreamReader(new ByteArrayInputStream(inputString.getBytes()))),
+            writer);
+
+    target.run();
+
+    writer.flush();
+
+    return outputStream.toString();
   }
 }
