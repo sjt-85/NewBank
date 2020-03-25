e8f4eef5bdf24c2ead09bdaad81a8b5763a1fdcc
tp757 <tp757@bath.ac.uk>
2020-03-25 12:48:25 +0000

    Added indent to align commands

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -47,13 +47,12 @@ public class NewBank {
     commands.add(
         "NEWACCOUNT <account type> <optional: account name> <optional: currency> \n"
             + "-> Creates a new account of specified type e.g. NEWACCOUNT \"Savings Account\" \"my savings\" EUR \n"
-            + "Standard currency is GBP, please specify an account name and currency to create an account with a different currency.");
+            + "   Standard currency is GBP, please specify an account name and currency to create an account with a different currency.");
     commands.add(
         "VIEWACCOUNTTYPE <account type> -> Prints details of specified account type e.g. VIEWACCOUNTTYPE \"Cash ISA\"");
     commands.add("HELP / COMMANDS -> Show command list");
     commands.add("LOGOUT -> Ends the current banking session and logs you out of NewBank.");
   }
-
 
   public static NewBank getBank() {
     return bank;
