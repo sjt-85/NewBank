73aebcc4a8e306d639d05c3bc5f941cea58d385a
tp757 <tp757@bath.ac.uk>
2020-03-26 01:01:24 +0000

    additional full stops added to command list and tests

--- newbank/server/NewBank.java
+++ newbank/server/NewBank.java
@@ -46,11 +46,11 @@ public class NewBank {
     commands.add("SHOWMYACCOUNTS -> Lists all of your active accounts.");
     commands.add(
         "NEWACCOUNT <account type> <optional: account name> <optional: currency> \n"
-            + "-> Creates a new account of specified type e.g. NEWACCOUNT \"Savings Account\" \"my savings\" EUR \n"
+            + "-> Creates a new account of specified type e.g. NEWACCOUNT \"Savings Account\" \"my savings\" EUR. \n"
             + "   Standard currency is GBP, please specify an account name and currency to create an account with a different currency.");
     commands.add(
-        "VIEWACCOUNTTYPE <account type> -> Prints details of specified account type e.g. VIEWACCOUNTTYPE \"Cash ISA\"");
-    commands.add("HELP / COMMANDS -> Show command list");
+        "VIEWACCOUNTTYPE <account type> -> Prints details of specified account type e.g. VIEWACCOUNTTYPE \"Cash ISA\".");
+    commands.add("HELP / COMMANDS -> Show command list.");
     commands.add("LOGOUT -> Ends the current banking session and logs you out of NewBank.");
   }
 
