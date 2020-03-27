7fa6115dd84fe7147e6085aeab049f532699aefd
tp757 <tp757@bath.ac.uk>
2020-03-27 00:00:20 +0000

    Updated account method toallow money in/out + added getter for balance

--- newbank/server/Account.java
+++ newbank/server/Account.java
@@ -3,7 +3,7 @@ package newbank.server;
 import java.math.BigDecimal;
 
 public class Account {
-  
+
   public enum AccountType {
     NONE {
       @Override
@@ -29,7 +29,7 @@ public class Account {
         return "Cash ISA";
       }
     };
-    
+
     public static AccountType getAccountTypeFromString(String s) {
       if (s != null) {
         for (AccountType a : AccountType.values()) {
@@ -41,42 +41,57 @@ public class Account {
       return AccountType.NONE;
     }
   }
-  
+
   private String accountName;
   private AccountType accountType;
-  private BigDecimal openingBalance;
+  private BigDecimal balance;
   private Currency currency;
 
   // allowing to enter the balance in double to make it easier to use but save for calculations
   public Account(AccountType accountType, String accountName, double openingBalance) {
     this.accountType = accountType;
     this.accountName = accountName;
-    this.openingBalance = convertDoubleToBigDecimal(openingBalance);
+    this.balance = convertDoubleToBigDecimal(openingBalance);
     this.currency = Currency.GBP;
   }
 
   public Account(AccountType accountType, String accountName, double openingBalance, Currency currency) {
     this.accountType = accountType;
     this.accountName = accountName;
-    this.openingBalance = convertDoubleToBigDecimal(openingBalance);
+    this.balance = convertDoubleToBigDecimal(openingBalance);
     this.currency = currency;
   }
-  
+
   public String toString() {
     return (accountType.toString() + ": " + accountName + ": " + openingBalance + " " + currency.name());
   }
-  
+
   public String getAccountName() {
     return this.accountName;
   }
-  
+
   public AccountType getAccountType() {
     return this.accountType;
+  }
+
+  public BigDecimal getBalance() {
+    return this.balance;
+  }
+
+  public void moneyIn(double amount){
+    BigDecimal in = convertDoubleToBigDecimal(amount);
+    this.balance = balance.add(in);
+  }
+
+  public void moneyOut(double amount){
+    BigDecimal out = convertDoubleToBigDecimal(- amount);
+    this.balance = balance.add(out);
   }
 
   private BigDecimal convertDoubleToBigDecimal(double amount) {
     BigDecimal bd = BigDecimal.valueOf(amount);
     return bd.setScale(2);
   }
+
 
 }
