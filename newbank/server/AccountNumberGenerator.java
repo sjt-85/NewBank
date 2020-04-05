8206a3234d7bc9165a562cad5432883291d47b76
st2180 <st2180@bath.ac.uk>
2020-04-05 01:38:26 +0100

    Bug fix: generate a new account number properly even after an account has been removed.

--- newbank/server/AccountNumberGenerator.java
+++ newbank/server/AccountNumberGenerator.java
@@ -1,7 +1,14 @@
 package newbank.server;
 
 public class AccountNumberGenerator {
-  public int getNextAccountNumber() {
-    return NewBank.getBank().getAccounts().keySet().stream().max(Integer::compareTo).get() + 1;
+
+  private Integer seed;
+
+  public synchronized int getNextAccountNumber() {
+    if (seed == null)
+      // only fall here the first time this method is called initialize the seed value
+      seed = NewBank.getBank().getAccounts().keySet().stream().max(Integer::compareTo).get();
+
+    return ++seed;
   }
 }
