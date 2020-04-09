96285de3867e088e5724ba56afbc305413f07af6
tp757 <tp757@bath.ac.uk>
2020-04-09 23:16:05 +0100

    updated description

--- newbank/server/Commands/OfferCommand.java
+++ newbank/server/Commands/OfferCommand.java
@@ -8,13 +8,16 @@ public class OfferCommand extends NewBankCommand {
 
   @Override
   public String getDescription() {
-    return "<account type> <optional: account name> <optional: currency> "
+    return "<amount> <interest rate as percentage> <max borrowing length in months> "
         + System.lineSeparator()
-        + "-> Offer a loan to borrowers eg OFFER \"Savings Account\" \"my savings\" EUR. "
+        + "-> Offer a loan to borrowers eg OFFER 500 10% 6. "
         + System.lineSeparator()
         + "   Standard currency is GBP, please specify an account name and currency to create an account with a different currency.";
   }
 
   @Override
-  public void run(NewBankCommandRequest request, NewBankCommandResponse response) {}
+  public void run(NewBankCommandRequest request, NewBankCommandResponse response) {
+
+
+  }
 }
