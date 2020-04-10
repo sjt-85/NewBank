4930ed30b93a8e11c43c9d39fa9634a10273b591
sjt-85 <sjt85@bath.ac.uk>
2020-04-10 20:14:10 +0100

    added missing braces

--- newbank/server/OfferNumberGenerator.java
+++ newbank/server/OfferNumberGenerator.java
@@ -5,13 +5,14 @@ public class OfferNumberGenerator {
   private Integer seed = null;
 
   public synchronized int getNextOfferNumber() {
-    if (seed == null)
+    if (seed == null) {
       // only fall here the first time this method is called initialize the seed value
       if (MicroLoanMarketPlace.getInstance().getOffers().size() > 0) {
         seed = MicroLoanMarketPlace.getInstance().getOffers().keySet().stream().max(Integer::compareTo).get();
       } else {
         seed = 0;
       }
+    }
 
     return ++seed;
   }
